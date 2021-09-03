694eba54d8dbcf7f50b0e7f764858a3023cfdc17
hartini03 <hartiniattari03@gmail.com>
2021-09-03 21:45:56 +0700

    Initial commit

---
+++ src/Cafe.java
@@ -0,0 +1,10 @@
+public class Cafe
+{
+    public static void main ( String args[] )
+    {
+     String[]  menu =
+             {"Moca","Americano","Cappucino","Redvelvet"};
+     System.out.println(menu[3]);
+    }
+
+}
